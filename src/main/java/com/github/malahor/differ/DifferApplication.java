3538646f7676d2493b43f688ec674cdb3dad001c
Malahor <malahor1610@gmail.com>
2024-05-18 10:30:22 +0200

    create project

---
+++ src/main/java/com/github/malahor/differ/DifferApplication.java
@@ -0,0 +1,13 @@
+package com.github.malahor.differ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DifferApplication.class, args);
+	}
+
+}
